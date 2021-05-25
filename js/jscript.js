2be3290087b54098b1b94a82cf0566398c679881
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2021-05-25 00:12:12 -0300

    Add files via upload

--- js/jscript.js
+++ js/jscript.js
@@ -1,7 +1,17 @@
             var ArrayTipo = new Array ("");
-            var ArrayTipo1 = new Array ("78.4");
-            var ArrayTipo2 = new Array ("75");
-            var ArrayTipo3 = new Array ("68");
+            var ArrayTipo1 = new Array ("92");
+            var ArrayTipo2 = new Array ("92");
+            var ArrayTipo3 = new Array ("85");
+            var ArrayTipo4 = new Array ("90");
+            var ArrayTipo5 = new Array ("96.5");
+            var ArrayTipo6 = new Array ("86");
+            var ArrayTipo7 = new Array ("85.7");
+            var ArrayTipo8 = new Array ("79");
+            var ArrayTipo9 = new Array ("84");
+            var ArrayTipo10 = new Array ("66.5");
+            var ArrayTipo11 = new Array ("76");
+            var ArrayTipo12 = new Array ("78.8");
+            var ArrayTipo13 = new Array ("75");
 
             function PopulaCombo(id_tipo)
             {
@@ -11,6 +21,26 @@
                     ArrayTipo = ArrayTipo2;
                 else if (id_tipo == 3)
                     ArrayTipo = ArrayTipo3;
+                else if (id_tipo == 4)
+                    ArrayTipo = ArrayTipo4;
+                else if (id_tipo == 5)
+                    ArrayTipo = ArrayTipo5;
+                else if (id_tipo == 6)
+                    ArrayTipo = ArrayTipo6;
+                else if (id_tipo == 7)
+                    ArrayTipo = ArrayTipo7;
+                else if (id_tipo == 8)
+                    ArrayTipo = ArrayTipo8;
+                else if (id_tipo == 9)
+                    ArrayTipo = ArrayTipo9;
+                else if (id_tipo == 10)
+                    ArrayTipo = ArrayTipo10;
+                else if (id_tipo == 11)
+                    ArrayTipo = ArrayTipo11;
+                else if (id_tipo == 12)
+                    ArrayTipo = ArrayTipo12;
+                else if (id_tipo == 13)
+                    ArrayTipo = ArrayTipo13;
                 else
                 {
                     Remove();
